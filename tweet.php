abff7387bcd25f0d832bb11d9c60120904ef16e1
Dorian Villet <gnutix@gmail.com>
2014-03-22 17:48:55 +0100

    Fix SensioLabs inspections.

--- tweet.php
+++ tweet.php
@@ -1,1 +1,1 @@
-<?php /*by sukei*/function p($l){$i=0;while($i++<$l){echo($i%3+$i%5==0?'FizzBuzz':($i%3==0?'Fizz':($i%5==0?'Buzz':$i))).PHP_EOL;}}p(100);
+<?php namespace sukei;function p($l){$i=0;while($i++<$l){echo($i%3+$i%5==0?'FizzBuzz':($i%3==0?'Fizz':($i%5==0?'Buzz':$i)))."\n";}}p(100);
