ca515c79850417425a295df0909026dff50728f8
Dorian Villet <gnutix@gmail.com>
2014-03-22 20:06:10 +0100

    Add empty test classes for missing tests.

---
+++ tests/Unit/GameDomain/Round/Step/StepResultTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit\GameDomain\Round\Step;
+
+/**
+ * StepResult Test
+ */
+class StepResultTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function testIsValid()
+    {
+        $this->markTestIncomplete();
+    }
+}
---
+++ tests/Unit/GameDomain/Rule/AbstractRuleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit\GameDomain\Rule;
+
+/**
+ * AbstractRule Test
+ */
+class AbstractRuleTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function testIsSatisfiedBy()
+    {
+        $this->markTestIncomplete();
+    }
+}
